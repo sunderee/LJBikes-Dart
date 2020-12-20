b0caf1dd2ff62ab36e358f6a72d406a5da4e4da4
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2020-12-20 15:19:25 +0100

    Minor changes

--- example/ljbikes_example.dart
+++ example/ljbikes_example.dart
@@ -2,6 +2,6 @@ import 'package:ljbikes/src/ljbikes_base.dart';
 
 void main() {
   retrieveAllStands()
-      .then((stands) => print(stands?.map((stand) => stand.location)))
+      .then((stands) => print(stands?.map((stand) => stand.name)))
       .catchError((error) => print(error));
 }
